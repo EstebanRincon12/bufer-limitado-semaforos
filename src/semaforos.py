9e1c9e4195223f3e60049ca38f25776008d23906
EstebanRincon12 <esteban.rincon@uptc.edu.co>
2023-05-07 19:17:08 -0500

    Se añadio funcionalidad detener

--- src/semaforos.py
+++ src/semaforos.py
@@ -2,6 +2,18 @@ from time import sleep
 import tkinter as tk
 from tkinter import ttk
 from threading import Thread, Semaphore
+
+
+s = Semaphore(1)
+buf = []
+producer_idx = 0
+consumer_idx = 0
+counter = 0
+producers = list()
+consumers = list()
+running = True
+buf_size = len(buf)
+
 """
     The class implements the "Producers and Consumers" problem using semaphores. 
     There is a shared region called buffer, which can store a limited number of 
@@ -14,120 +26,123 @@ from threading import Thread, Semaphore
         Gina Castillo       gina.castillo01@uptc.edu.co
         Zulma Samaca        zulma.samaca@uptc.edu.co
 """
+class Semaphore:
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.ventana.geometry('800x500')
+        self.ventana.title('Productores y Consumidores con semáforos')
 
-ventana = tk.Tk()
-ventana.geometry('800x500')
-ventana.title('Productores y Consumidores con semáforos')
+        self.labelTitle = tk.Label(self.ventana, text='Productores y Consumidores con semáforos')
+        self.labelNumberConsumer = ttk.Label(self.ventana, text='N° Consumidores')
+        self.labelNumberProductors = ttk.Label(self.ventana, text='N° Productores')
+        self.labelSizeBuffer = ttk.Label(self.ventana, text='Tamaño Buffer')
+        self.labelBuffer = ttk.Label(self.ventana, text='Buffer')
 
-labelTitle = tk.Label(ventana, text='Productores y Consumidores con semáforos')
-labelNumberConsumer = ttk.Label(ventana, text='N° Consumidores')
-labelNumberProductors = ttk.Label(ventana, text='N° Productores')
-labelSizeBuffer = ttk.Label(ventana, text='Tamaño Buffer')
-labelBuffer = ttk.Label(ventana, text='Buffer')
+        self.entryProductos = ttk.Entry(self.ventana)
+        self.entryConsumers = ttk.Entry(self.ventana)
+        self.entrySizeBuffer = ttk.Entry(self.ventana)
 
-entryProductos = ttk.Entry(ventana)
-entryConsumers = ttk.Entry(ventana)
-entrySizeBuffer = ttk.Entry(ventana)
+    
 
-def startSimulation():
+        self.buttonStart = ttk.Button(self.ventana, text='Iniciar', command=self.startSimulation)
+        self.buttonStop = ttk.Button(self.ventana, text='Detener', command=self.stopSimulation)
 
-    global buf, buf_size
-    buf_size = int(entrySizeBuffer.get())
-    buf =[" "] * buf_size
-    numberConsumers = int(entryConsumers.get())
-    numberProductors = int(entryProductos.get())
-    createConsumers(numberConsumers)
-    createProducer(numberProductors)
+        self.canvas = tk.Canvas(master=self.ventana, width=700, height=100)
 
-buttonStart = ttk.Button(ventana, text='Iniciar', command=startSimulation)
+        self.ventana.rowconfigure(0, weight=1)
+        self.ventana.rowconfigure(1, weight=1)
+        self.ventana.columnconfigure(0, weight=1)
+        self.ventana.columnconfigure(1, weight=1)
+        self.ventana.rowconfigure(2, weight=1)
+        self.ventana.rowconfigure(3, weight=1)
+        self.ventana.rowconfigure(4, weight=1)
+        self.ventana.rowconfigure(5, weight=1)
+        self.ventana.rowconfigure(6, weight=1)
+        self.ventana.rowconfigure(7, weight=1)
+        self.ventana.rowconfigure(8, weight=1)
+        self.ventana.rowconfigure(9, weight=1)
+        self.ventana.columnconfigure(2, weight=1)
+        self.ventana.columnconfigure(3, weight=1)
+        self.ventana.columnconfigure(3, weight=1)
+        self.ventana.columnconfigure(4, weight=1)
+        self.ventana.columnconfigure(5, weight=1)
+        self.ventana.columnconfigure(6, weight=1)
 
-canvas = tk.Canvas(master=ventana, width=700, height=100)
+        self.labelTitle.grid(row=0, column=0, columnspan=7)
+        self.labelNumberProductors.grid(row=1, column=0)
+        self.labelNumberConsumer.grid(row=1, column=2)
+        self.labelSizeBuffer.grid(row=1, column=4)
+        self.entryProductos.grid(row=1, column=1)
+        self.entryConsumers.grid(row=1, column=3)
+        self.entrySizeBuffer.grid(row=1, column=5)
+        self.buttonStart.grid(row=1, column=6)
+        self.buttonStop.grid(row=2, column=6)
+        self.labelBuffer.grid(row=2, column=0, columnspan=7, rowspan=7)
+        
 
-ventana.rowconfigure(0, weight=1)
-ventana.rowconfigure(1, weight=1)
-ventana.columnconfigure(0, weight=1)
-ventana.columnconfigure(1, weight=1)
-ventana.rowconfigure(2, weight=1)
-ventana.rowconfigure(3, weight=1)
-ventana.rowconfigure(4, weight=1)
-ventana.rowconfigure(5, weight=1)
-ventana.rowconfigure(6, weight=1)
-ventana.rowconfigure(7, weight=1)
-ventana.rowconfigure(8, weight=1)
-ventana.rowconfigure(9, weight=1)
-ventana.columnconfigure(2, weight=1)
-ventana.columnconfigure(3, weight=1)
-ventana.columnconfigure(3, weight=1)
-ventana.columnconfigure(4, weight=1)
-ventana.columnconfigure(5, weight=1)
-ventana.columnconfigure(6, weight=1)
 
-labelTitle.grid(row=0, column=0, columnspan=7)
-labelNumberProductors.grid(row=1, column=0)
-labelNumberConsumer.grid(row=1, column=2)
-labelSizeBuffer.grid(row=1, column=4)
-entryProductos.grid(row=1, column=1)
-entryConsumers.grid(row=1, column=3)
-entrySizeBuffer.grid(row=1, column=5)
-buttonStart.grid(row=1, column=6)
-labelBuffer.grid(row=2, column=0, columnspan=7, rowspan=7)
+    def startSimulation(self):
+        global running, producer_idx, consumer_idx, counter, buf, buf_size
+        running = True
+        producer_idx = 0
+        consumer_idx = 0
+        counter = 0
+        buf_size = int(self.entrySizeBuffer.get())
+        buf =[" "] * buf_size
+        numberConsumers = int(self.entryConsumers.get())
+        numberProductors = int(self.entryProductos.get())
+        self.createConsumers(numberConsumers)
+        self.createProducer(numberProductors)
 
-s = Semaphore(1)
-buf = []
-producer_idx = 0
-consumer_idx = 0
-counter = 0
-producers = list()
-consumers = list()
-running = True
-buf_size = len(buf)
+    def produce(self):
+        global producer_idx, counter, buf
+        while running:
+            with s:
+                if counter == buf_size:  # full
+                    continue
+                buf[producer_idx] = "x"
+                producer_idx = (producer_idx + 1) % buf_size
+                print("{} <= produced 'x' at index='{}'".format(buf, producer_idx))
+                self.labelBuffer.config(text="{} => consumed '{}' at index='{}'".format(buf, buf[consumer_idx], consumer_idx))
+                counter += 1
+            sleep(1)
 
-def produce():
-    global producer_idx, counter, buf
-    while running:
-        with s:
-            if counter == buf_size:  # full
-                continue
-            buf[producer_idx] = "x"
-            producer_idx = (producer_idx + 1) % buf_size
-            print("{} <= produced 'x' at index='{}'".format(buf, producer_idx))
-            labelBuffer.config(text="{} => consumed '{}' at index='{}'".format(buf, buf[consumer_idx], consumer_idx))
-            counter += 1
-        sleep(1)
-
-def consume():
-    global consumer_idx, counter, buf
-    while running:
-        with s:
-            if counter == 0:  # empty
-                continue
-            buf[consumer_idx] = " "
-            consumer_idx = (consumer_idx + 1) % buf_size
-            print("{} => consumed '{}' at index='{}'".format(buf, buf[consumer_idx], consumer_idx))
-            counter -= 1
-        sleep(1)
+    def consume(self):
+        global consumer_idx, counter, buf
+        while running:
+            with s:
+                if counter == 0:  # empty
+                    continue
+                buf[consumer_idx] = " "
+                consumer_idx = (consumer_idx + 1) % buf_size
+                print("{} => consumed '{}' at index='{}'".format(buf, buf[consumer_idx], consumer_idx))
+                counter -= 1
+            sleep(1)
  
-def createProducer(quantity):
-    for i in range(0,quantity):
-        producer = Thread(target=produce)
-        producer.start()
-        producers.append(producer)
+    def createProducer(self, quantity):
+        for i in range(0,quantity):
+            producer = Thread(target=self.produce)
+            producer.start()
+            producers.append(producer)
 
-def createConsumers(quantity):
-    for i in range(0, quantity):
-        consumer = Thread(target=consume)
-        consumer.start()
-        consumers.append(consumer)
+    def createConsumers(self, quantity):
+        for i in range(0, quantity):
+            consumer = Thread(target=self.consume)
+            consumer.start()
+            consumers.append(consumer)
 
-def stopSimulation():
-    global running
-    running = False
-    ventana.destroy()
-
-
-ventana.protocol("WM_DELETE_WINDOW", stopSimulation)
-
-ventana.mainloop()
+    def stopSimulation(self):
+        global running, producers, consumers
+        producers = []
+        consumers = []
+        running = False
 
 
+    def closeSimulation(self):
+        global running
+        running = False
+        self.ventana.destroy()
 
+view = Semaphore()
+view.ventana.protocol("WM_DELETE_WINDOW", view.closeSimulation)
+view.ventana.mainloop()
